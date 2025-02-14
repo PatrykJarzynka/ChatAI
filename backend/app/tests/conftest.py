2ae7313cdcafcbb2c8e445917b0bbf17b1c9181b
Patryk Jarzynka <patryk.jarzynka@codeconcept.pl>
2025-02-14 10:58:42 +0100

    Removed unused constant

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -10,7 +10,6 @@ from app.main import app
 from app.services.auth.jwt_service import JWTService
 
 MOCKED_TOKEN = Token(access_token="fake_token", token_type="bearer")
-MOCKED_HASH = 'someHashedPassword'
 
 
 @pytest.fixture(name='session')
