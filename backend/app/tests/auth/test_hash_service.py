277a89fb3722a8132bfe3e225c2399eb9299c018
Patryk Jarzynka <patryk.jarzynka@codeconcept.pl>
2025-02-14 10:33:10 +0100

    Created tests for hash service. Moved routers tests to separate catalog

---
+++ backend/app/tests/auth/test_hash_service.py
@@ -0,0 +1,28 @@
+import pytest
+
+from app.services.auth.hash_service import HashService
+
+
+@pytest.fixture
+def hash_service():
+    return HashService()
+
+
+def test_hash_password(hash_service):
+    password = "TestPassword123"
+
+    hashed_password = hash_service.hash_password(password)
+
+    assert hashed_password != password, "Hashed password should not be the same as the original password"
+
+    assert hashed_password.startswith('$2b$'), "Hashed password format should start with $2b$ (bcrypt)"
+
+
+def test_verify_password(hash_service):
+    password = "TestPassword123"
+
+    hashed_password = hash_service.hash_password(password)
+
+    assert hash_service.verify_password(password, hashed_password) == True, "Password verification failed"
+
+    assert hash_service.verify_password("WrongPassword", hashed_password) == False, "Password verification failed"
