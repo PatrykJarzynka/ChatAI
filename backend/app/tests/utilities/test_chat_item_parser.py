78aa467d0cf915de3c5cbbe321250020c778b5bd
Patryk Jarzynka <patryk.jarzynka@codeconcept.pl>
2025-04-22 17:21:09 +0200

    Implemented tests for chat item parser

---
+++ backend/app/tests/utilities/test_chat_item_parser.py
@@ -0,0 +1,24 @@
+import pytest
+from utilities.chat_items_parser import ChatItemsParser
+from db_models.chat_item_model import ChatItem
+from llama_index.core.base.llms.types import ChatMessage
+
+@pytest.fixture
+def chat_items_parser():
+    return ChatItemsParser()
+
+
+def test_parse_to_chat_messages(chat_items_parser: ChatItemsParser):
+    mocked_chat_items = [ChatItem(user_message='Hello.', bot_message='Hello, how are you?'), 
+                         ChatItem(user_message='Hello2.', bot_message='Hello, how are you2?')]
+    
+    expected_chat_messages = [ChatMessage(content='Hello.', role='user'),ChatMessage(content='Hello, how are you?', role='assistant'),
+                              ChatMessage(content='Hello2.', role='user'),ChatMessage(content='Hello, how are you2?', role='assistant'),]
+
+    chat_messages = chat_items_parser.parse_to_chat_messages(mocked_chat_items)
+
+    assert chat_messages == expected_chat_messages
+
+
+
+
