972c1cce608f2c1e6773885b328cdbab94856ab8
Patryk Jarzynka <patryk.jarzynka@codeconcept.pl>
2025-04-22 10:18:48 +0200

    Added comments to auth service tests

--- backend/app/tests/auth/test_auth_service.py
+++ backend/app/tests/auth/test_auth_service.py
@@ -32,19 +32,19 @@ def wrong_type_request():
     return MockRequest()
 
 def test_get_token_from_header(get_auth_service: AuthService, header_request):
-    assert get_auth_service.get_token_from_header(header_request) == 'XYZ'
+    assert get_auth_service.get_token_from_header(header_request) == 'XYZ', 'Tokens are not the same'
 
 def test_lack_of_token(get_auth_service: AuthService, no_header_request):
     with pytest.raises(HTTPException) as no_token_exception:
         get_auth_service.get_token_from_header(no_header_request)
-        assert no_token_exception.value.status_code == status.HTTP_401_UNAUTHORIZED
-        assert no_token_exception.value.detail == 'Authorization header missing'
+        assert no_token_exception.value.status_code == status.HTTP_401_UNAUTHORIZED, 'Status code should be 401'
+        assert no_token_exception.value.detail == 'Authorization header missing', 'Exception message should be equal "Authorization header missing"'
 
 def test_wrong_token_type(get_auth_service: AuthService, wrong_type_request):
     with pytest.raises(HTTPException) as no_token_exception:
         get_auth_service.get_token_from_header(wrong_type_request)
-        assert no_token_exception.value.status_code == status.HTTP_401_UNAUTHORIZED
-        assert no_token_exception.value.detail == 'Invalid token type'
+        assert no_token_exception.value.status_code == status.HTTP_401_UNAUTHORIZED,'Status code should be 401'
+        assert no_token_exception.value.detail == 'Invalid token type', 'Exception message should be equal "Invalid token type"'
 
 
 
@@ -70,5 +70,5 @@ def test_exception_handler(get_auth_service: AuthService, exception, expected_st
     with pytest.raises(HTTPException) as expected_exception:
         get_auth_service.handle_token_exceptions(raise_exception)()
         
-    assert expected_exception.value.detail == expected_message
-    assert expected_exception.value.status_code == expected_status
+    assert expected_exception.value.detail == expected_message, f'Exception message should be equal {expected_message}'
+    assert expected_exception.value.status_code == expected_status, f'Status code should be {expected_status}'
