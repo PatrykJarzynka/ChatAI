ec0b384a4b5cdea9b127112792384bea515076c2
Patryk Jarzynka <patryk.jarzynka@codeconcept.pl>
2025-04-23 13:57:26 +0200

    Implemented test for fetching jwks in micorosft public keys provider

--- backend/app/tests/utilities/test_microsoft_public_keys_provider.py
+++ backend/app/tests/utilities/test_microsoft_public_keys_provider.py
@@ -21,3 +21,20 @@ def test_get_openid_config(microsoft_public_keys_provider: MicrosoftPublicKeysPr
 
     mock_get.assert_called_once_with(expected_url)
     assert response == mock_json, 'Wrong response'
+
+def test_get_jwks_by_config(microsoft_public_keys_provider: MicrosoftPublicKeysProvider):
+    mock_url = 'https://login.microsoftonline.com'
+    mock_keys = [{"mock_key": "mock_value"}, {"mock_key2": "mock_value2"}]
+
+    mock_openid_config = {"jwks_uri": mock_url}
+    mock_json = {"keys": mock_keys}
+
+    with patch('requests.get') as mock_get:
+        mock_response = Mock()
+        mock_response.json.return_value = mock_json
+        mock_get.return_value = mock_response
+
+        response = microsoft_public_keys_provider.get_jwks_by_config(mock_openid_config)
+    
+    mock_get.assert_called_once_with(mock_url)
+    assert response == mock_keys, 'Wrong response'
